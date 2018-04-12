9d488894cfb9c60419dfd083c0691861f0cc4021
marinkaf <37780870+marinkaf@users.noreply.github.com>
2018-04-12 23:34:41 +0300

    2step

--- features/area_skreen.feature
+++ features/area_skreen.feature
@@ -1,27 +1,47 @@
 @area
 Feature: User is able convert area units
-  @menu
-  Scenario: User is able to dismiss help
-    Given I hand on help popup
-    When I click on Got in button
-    Then I land on Area screen
-@wit
-  Scenario: User is able to swap value
+
+  Background:
     Given I click on Got in button
-    And I see "Sq Kilometre" in From header
+
+#  Scenario: User is able to dismiss help
+#    Given I land on help popup
+#    When I click on Got in button
+#    Then I land on Area screen
+
+  Scenario: User is able to swap value
+#    Given I click on Got in button
+    Given I see "Sq Kilometre" in From header
     And I see "Sq Metre" in to header
     When I click on Swap button
     Then I see "Sq Metre" in From header
     And I see "Sq Kilometre" in to header
 
   Scenario Outline: User is able to convert default units
-    Given I click on Got in button
-    And I click on Clear button
-    When I enter "1" to From field
-    Then I get "1000000" in To field
+#    Given I click on Got in button
+    Given I click on Clear button
+    When I enter "<target>" to From field
+    Then I get "<result>" in To field
 
     Examples:
      |target|result|
      |1     |1000000|
      |2     |2000000|
      |3     |3000000|
+
+  Scenario: User is able to use soft keyboardti enter values.
+#           Пользователь вводит значения с использованием клавиатуры
+    Given I click on Clear button
+    When I click on From field
+#        Я кликаю по полю ОТ
+    And I press "30" on soft keybord
+#       Я нажимаю "3" на клавиатуре
+    Then I get "30.000.000" in To field
+#        Я получаю "30000" в поле Результат
+  @wip
+  Scenario:
+    When I select "Кв. сантиметр" from left column
+    Then I see "Кв. сантиметр" in From header
+    And I get "0,0001" in To field
+
+    
--- features/step_definitions/area screen steps.rb
+++ features/step_definitions/area screen steps.rb
@@ -1,35 +1,68 @@
-Given(/^I hand on help popup$/) do
-  puts("User lands on Help popup")
+Given(/^I land on help popup$/) do
+  # puts("User lands on Help popup")
+  text("Помощь")
 end
 
 When(/^I click on Got in button$/) do
-  puts("User clicks on Got it button")
+  # find_element(text: "Got It").click
+  find_element(id: "button1").click
+  # puts("User clicks on Got it button")
 end
 
 Then(/^I land on Area screen$/) do
-  puts("User lands on Area screen")
+  text("Площадь")
+  # puts("User lands on Area screen")
 end
 
 When(/^I click on Swap button$/) do
-  puts("User clicks Swap button")
+  find_element(id: "fab").click
 end
 
 Then(/^I see "([^"]*)" in From header$/) do |value|
-  puts("From header value is " + value)
+  actual_value = find_element(id: "header_text_unit_from").text
+  puts("Exexted value is #{value}")
+  puts("Actual_value is #{actual_value}")
+  # puts("From header value is " + value)
 end
 
 And(/^I see "([^"]*)" in to header$/) do |value|
-  puts("To header value is #{value}")
+  actual_value = find_element(id: "header_text_unit_to").text
+  puts("Exexted value is #{value}")
+  puts("Actual_value is #{actual_value}")
 end
 
 And(/^I click on Clear button$/) do
-  puts ("User clicks on Clear button")
+  find_element(id: "menu_clear").click
+  # puts ("User clicks on Clear button")
 end
 
 When(/^I enter "([^"]*)" to From field$/) do |value|
- puts ("User entered value #{value}")
-end
+  find_element(id: "header_value_from").send_keys(value)
+  end
 
 Then(/^I get "([^"]*)" in To field$/) do |value|
-  puts ("User sees #{value} in result field")
+  actual_value = find_element(id: "header_value_to").text
+  # puts("Exexted value is #{value}")
+  # puts("Actual_value is #{actual_value}")
+  if actual_value != value
+    fail("Expected value is #{value}, but actual value was #{actual_value}")
+  end
 end
+
+When(/^I click on From field$/) do
+  find_element(id: "header_value_from").click
+end
+
+And(/^I press "([^"]*)" on soft keybord$/) do |value|
+  digits = value.split("")
+  digits.each do |key|
+   digit = Integer(key)
+   press_keycode 7 + digit
+end
+end
+
+When(/^I select "([^"]*)" from left column$/) do |value|
+  # find_element(id:"radio_group_from").find_element(xpath: "/android.widget.RadioButton[@text='Кв. сантиметр']/")
+  find_element(id:"radio_group_from").find_element(xpath: "//android.widget.RadioButton[@text='#{value}']").click
+end
+
